be9a6afe06ede2a267c98e4d151268a14739607c
Aden Jo <114706623+adenjo6@users.noreply.github.com>
2025-04-22 14:21:30 -0700

    Add files via upload

--- L3B/src/main.c
+++ L3B/src/main.c
@@ -1,8 +1,8 @@
 /*
  * ECE 153B
  * 
- * Name(s):
- * Section:
+ * Name(s): Aden, Vishal
+ * Section: Tuesday 6pm
  * Lab: 3B
  */
 
@@ -14,21 +14,45 @@ void SERVO_Pin_Init(void);
 void TIM5_CH1_Init(void);
 
 void LED_Pin_Init(void){
-  // Enable GPIO Clocks
+	 // Enable GPIO Clocks
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	
 	// Initialize Green LED
-	// GPIO Mode
-	// Alternative Function
+	GPIOA->MODER &= ~(GPIO_MODER_MODE5); //Clear the mode register
+	GPIOA->MODER  |= GPIO_MODER_MODE5_1; //Set to Alternative Function
+	
 	//Set I/O output speed value as very high speed
+	GPIOA->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED5);	
+	
 	// GPIO Push-Pull: No pull-up, no pull-down
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD5); //Set pull-up/pull-down to no pull-up/pull-down
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5); //Set output type to push-pull
+	
+	//configure and selct alternative function for PA5 (double check)
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL5_0);
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL5_0;
+	
+	
+	
+	
+
 }
 	
 
 void TIM2_CH1_Init(void){
 	// Enable the timer clock
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
 	// Counting direction: 0 = up-counting, 1 = down-counting
+	TIM2->CR1 &= ~(TIM_CR1_DIR);
+	
   // Prescaler
+	
   // Auto-reload
+	TIM2->CR1 |= TIM_CR1_ARPE;
+
 	// Disable output compare mode
+	TIM2->CCMR1
+	
 	// PWM mode 1
 	// Output 1 preload enable
 	// Select output polarity: active high
