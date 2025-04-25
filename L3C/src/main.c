e6c9b80c15072acc4405e8c0ad9dbadf3cf9be08
Vishal S. <67886538+VishalSeeni-108@users.noreply.github.com>
2025-04-24 19:01:25 -0700

    Add files via upload

--- L3C/src/main.c
+++ L3C/src/main.c
@@ -53,7 +53,7 @@ void Input_Capture_Setup() {
 	//In the DMA/Interrupt enable register, enable both interrupt and DMA requests. In
 	//addition, enable the update interrupt
 	TIM4->DIER |= TIM_DIER_CC1IE; 
-	TIM4->DIER |= TIM_DIER_UDE; //double check this
+	TIM4->DIER |= TIM_DIER_CC1DE; //double check this
 	TIM4->DIER |= TIM_DIER_UIE; 
 	
 	//Enable update generation in the event generation register
@@ -80,7 +80,7 @@ void TIM4_IRQHandler(void) {
 		TIM4->SR &= ~TIM_SR_UIF; // clear interrupt flag
 		overflowCount++;
     } else if (TIM4->SR & TIM_SR_CC1IF) { // Channel #1
-        TIM4->SR &= ~ TIM_SR_CC1IF; // clear interrupt flag
+        TIM4->SR &= ~TIM_SR_CC1IF; // clear interrupt flag
         lastValue = currentValue;
         currentValue = TIM4->CCR1;
         timeInterval = currentValue - lastValue + overflowCount * (MAX_TIMER + 1);
@@ -95,12 +95,12 @@ void Trigger_Setup() {
 	//Configure PA9 to be used as alternative function TIM1_CH2
 	GPIOA->MODER &= ~(GPIO_MODER_MODE9); //Clear the mode register
 	GPIOA->MODER |= GPIO_MODER_MODE9_1; //Set to Alternative Function
-	GPIOA->AFR[0] &= ~(GPIO_AFRH_AFSEL9); //configure and select alternative function for PA5 
-	GPIOA->AFR[0] |= GPIO_AFRH_AFSEL9_0; 
+	GPIOA->AFR[1] &= ~(GPIO_AFRH_AFSEL9); //configure and select alternative function for PA5 
+	GPIOA->AFR[1] |= GPIO_AFRH_AFSEL9_0; 
 	//Set PA9 to no pull-up, no pull-down 
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD9); //Set pull-up/pull-down to no pull-up/pull-down
 	//Set the output type of PA9 to push-pull
-	GPIOA->OTYPER &= GPIO_OTYPER_OT9; 
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT9); 
 	//Set PA9 to very high output speed
 	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED9;
 	
@@ -117,13 +117,13 @@ void Trigger_Setup() {
 
 	//Set the CCR value that will trigger the sensor
 	TIM1->CCR2 = 10; //Double check and come back to this
-	
+
 	//In the capture/compare mode register, set the output compare mode such that the timer
 	//operates in PWM Mode 1 and enable output compare preload.
-	TIM1->CCMR1 &= ~(TIM_CCMR1_OC2M); 
+	TIM1->CCMR1 &= ~(TIM_CCMR1_OC1M); 
 	TIM1->CCMR1 |= TIM_CCMR1_OC2M_1; //Set the output compare mode bits to PWM mode 1
 	TIM1->CCMR1 |= TIM_CCMR1_OC2M_2;  //Make sure this is the right mode register
-	TIM1->CCMR1 &= TIM_CCMR1_OC2PE;  //Enable output compare preload
+	TIM1->CCMR1 |= TIM_CCMR1_OC2PE;  //Enable output compare preload
 	
 	//Enable the output in the capture/compare enable register. (Assuming CC1, check with TA)
 	TIM1->CCER	|= TIM_CCER_CC2E;
@@ -141,7 +141,7 @@ void Trigger_Setup() {
 	TIM1->SR &= ~(TIM_SR_UIF); 
 	
 	//Set the direction of the counter and enable the counter in the control register
-	TIM1->CR1 &= ~(TIM_CR1_DIR); //Upcounting, check with TA what direction should be
+	TIM1->CR1 |= (TIM_CR1_DIR); //Upcounting, check with TA what direction should be
 	TIM1->CR1 |= TIM_CR1_CEN; 
 }
 
